c045c628f6c898075f741a5110e71167ef59e1f2
Akshay Dudhane <akshay.aad16@gmail.com>
2023-05-30 21:04:46 +0400

    Update Grayscale_denoising_testing.py

--- Burst De-noising/Grayscale_denoising_testing.py
+++ Burst De-noising/Grayscale_denoising_testing.py
@@ -18,10 +18,11 @@ from Network import Burstormer
 
 ################# Utils ##############################################
 import lpips
-lpips_fn = lpips.LPIPS(net='alex')
 from pytorch_msssim import ssim
 from utils.metrics import PSNR
-psnr_fn = PSNR()
+psnr_fn = PSNR(boundary_ignore=2)
+loss_fn_alex = lpips.LPIPS(net='alex')
+
 
 ######################################################################
 
@@ -36,7 +37,6 @@ args = parser.parse_args()
 ######################################### Load Burstormer ####################################################
 
 model = Burstormer(mode='grayscale').load_from_checkpoint(args.weights, mode='grayscale')
-model = model
 model.cuda()
 model.summarize()
 
@@ -51,48 +51,67 @@ if not os.path.exists(args.result_dir):
 
 gray_testset = GrayscaleDenoiseTestSet(root=args.input_dir, noise_level=8)
 test_loader = DataLoader(gray_testset, batch_size=1, pin_memory=True)        
-process_fn = DenoisingPostProcess(return_np=True)
+process_fn = DenoisingPostProcess(return_np=False)
 
 #####################################################################
 
+
+score = 0
+PSNR = []
+SSIM = []
+LPIPS = []
+
+
 for i, data in enumerate(test_loader):
     
-    burst, labels, info = data            
-    noise_estimate = info['sigma_estimate']        
+    burst, labels, info = data
+    
+    noise_estimate = info['sigma_estimate']
+    
+    if noise_estimate.dim() == 4:
+        noise_estimate_re = noise_estimate.unsqueeze(1).repeat(1, burst.shape[1], 1, 1, 1)
+    else:
+        noise_estimate_re = noise_estimate
 
-    burst = burst[:, :, :, :200, :316]
-    labels = labels[:, :, :200, :316]
-    noise_estimate = noise_estimate[:, :, :, :200, :316]
+    burst = torch.cat((burst, noise_estimate_re), dim=2)
+    
+    burst = burst[:, :, :, :200, :320]
+    labels = labels[:, :, :200, :320]
 
     burst = burst.cuda()
     labels = labels.cuda()
-    noise_estimate = noise_estimate.cuda()
-                
+             
     with torch.no_grad():
-        output = model(burst, noise_estimate)
-    output = output.clamp(0.0, 1.0)
+        output = model(burst)
+        output = output.clamp(0.0, 1.0)
     
     ### PSNR computation
-    PSNR = psnr_fn(output, labels).cpu().numpy()
+    output = process_fn.process(output, info)
+    labels = process_fn.process(labels, info)
+          
+    PSNR.append(psnr_fn(output, labels).cpu().numpy() )
     
     ### LPIPS computation
     var1 = 2*output-1
     var2 = 2*labels-1
-    LPIPS = lpips_fn(var1.cpu(), var2.cpu())
-    LPIPS = torch.squeeze(LPIPS).detach().numpy()
+    LPIPS.append(torch.squeeze(loss_fn_alex(var1.cpu(), var2.cpu())).detach().numpy())
     
-    ### SSIM computation            
-    SSIM = ssim(output*255, labels*255, data_range=255, size_average=True)
-    SSIM = torch.squeeze(SSIM).cpu().detach().numpy()
+    ### SSIM computation
+    SSIM.append(torch.squeeze(ssim(output*255, labels*255, data_range=255, size_average=True)).cpu().detach().numpy())          
     
-    eval_Par = 'Evaluation Measures for Burst {:d} ::: PSNR is {:0.3f}, SSIM is {:0.3f} and LPIPS is {:0.3f} \n'.format(i, PSNR, SSIM, LPIPS)
-    print(eval_Par)       
+    noisy_input = burst[0,0, :1].permute(1,2,0).contiguous().cpu().numpy()            
+    output = output[0].permute(1,2,0).contiguous().cpu().numpy()            
+    labels = labels[0].permute(1,2,0).contiguous().cpu().numpy()
     
-    noisy_input = torch.mean(burst, 1)
-    noisy_input = process_fn.process(noisy_input[0].cpu(), info)            
-    output = process_fn.process(output[0].cpu(), info)            
-    labels = process_fn.process(labels[0].cpu(), info)
-    
-    output = np.concatenate((noisy_input, output, labels), axis=1)
+    output = np.concatenate((noisy_input, output, labels), axis=1)*255
     
     cv2.imwrite('{}/{}.png'.format(args.result_dir, str(i)), output)
+    
+    i+=1
+    
+Average_PSNR = sum(PSNR)/len(PSNR)
+Average_SSIM = sum(SSIM)/len(SSIM)
+Average_LPIPS = sum(LPIPS)/len(LPIPS)
+average_eval_par = '\nAverage Evaluation Measures ::: PSNR is {:0.3f}, SSIM is {:0.3f} and LPIPS is {:0.3f}\n'.format(Average_PSNR, Average_SSIM, Average_LPIPS)
+        
+print(average_eval_par)
